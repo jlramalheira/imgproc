afc5fce2c20db5a9f2c4aae1b5ab5b84c0625da9
Max <max.roecker@gmail.com>
2016-08-15 13:04:28 -0300

    Added trab4.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@
 import numpy as np
 import cv2
 
+def gamma_correction(img, gamma):
+    lut = (((np.arange(256, dtype=np.float32) / 255) ** gamma) * 255).astype(np.uint8)
+    result = lut[img]
+    return result
+
 
 def invert_image(img):
     return img[::-1, ::-1]
@@ -19,3 +24,11 @@ def create_diagonal_gradient(height, width):
 def bland(img1, img2):
     img = (np.uint8)((img1 + img2) / 2)
     return img
+
+def hsv_disc(size=512):
+    disc = np.zeros([size, size, 3])
+    (x, y, z) = np.indices(disc)
+    row_indexes = x - (size / 2)
+    col_indexes = y - (size / 2)
+    disc[1] = np.arctan2(col_indexes, row_indexes)
+    return disc;
---
+++ test/ex04.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+import numpy as np
+import cv2
+import sys
+sys.path.insert(0, '../src')
+import main
+
+# GRAY SCALE
+img_gray = cv2.imread(sys.argv[1], 0)
+
+result1 = main.gamma_correction(img_gray, 0.5)
+result2 = main.gamma_correction(img_gray, 1)
+result3 = main.gamma_correction(img_gray, 2)
+
+result = np.hstack((np.hstack((result1, result2)), result3))
+
+cv2.imshow('img_gray', result)
+cv2.waitKey(0)
+
+# RGB
+img_rgb = cv2.imread(sys.argv[1])
+
+result1 = main.gamma_correction(img_rgb, 0.5)
+result2 = main.gamma_correction(img_rgb, 1)
+result3 = main.gamma_correction(img_rgb, 2)
+
+result = np.hstack((np.hstack((result1, result2)), result3))
+
+cv2.imshow('img_rgb', result)
+cv2.waitKey(0)
+
+# HSI
+img_hsv = cv2.cvtColor(cv2.imread(sys.argv[1]), cv2.COLOR_BGR2HSV)
+
+h = img_hsv[:,:,0]
+s = img_hsv[:,:,1]
+v = img_hsv[:,:,2]
+
+v1 = main.gamma_correction(v, 0.5)
+v2 = main.gamma_correction(v, 1)
+v3 = main.gamma_correction(v, 2)
+
+result1 = cv2.cvtColor(np.dstack((h,s,v1)), cv2.COLOR_HSV2BGR)
+result2 = cv2.cvtColor(np.dstack((h,s,v2)), cv2.COLOR_HSV2BGR)
+result3 = cv2.cvtColor(np.dstack((h,s,v3)), cv2.COLOR_HSV2BGR)
+
+result = np.hstack((np.hstack((result1, result2)), result3))
+
+cv2.imshow('img_hsv', result)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
